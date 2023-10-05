bdc270a36e3fc72dc324d9e4bb7549272a2ebc79
R3D0XX <dr.doenges@tamsjadi.com>
2023-10-05 17:19:58 +0200

    first_commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+
+const app = express();
+
+const port = process.env.PORT || 5001
+
+app.listen(port, () => {
+    console.log('Server is running', port);
+    console.log('server is running', server is running)
+
+});
