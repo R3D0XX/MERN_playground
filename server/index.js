a5301acce0e359c8172e107ca45c51de5f8f4034
R3D0XX <dr.doenges@tamsjadi.com>
2023-10-09 16:10:03 +0200

    servercfg_working

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 import express from "express";
-
+import colors from "colors"
 
 
 const app = express();
 
 const port = process.env.PORT || 5001
 
+app.use(express.json());
+app.use(colors());
+app.use(
+    express.urlencoded({
+        extended: true;
+    })
+)
+
+
 app.listen(port, () => {
-    console.log('Server is running', port);
-    console.log('server is running', server is running)
+    console.log('Server is running.'.bgGreen, port);
+
+
 
 });
