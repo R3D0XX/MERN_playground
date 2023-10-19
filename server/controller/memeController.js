d8a5bba214dc9c6ead2f291ba9d5650755e22610
R3D0XX <dr.doenges@tamsjadi.com>
2023-10-19 16:42:27 +0200

    added some statuscodes

--- server/controller/memeController.js
+++ server/controller/memeController.js
@@ -2,12 +2,21 @@ import mongoose from "mongoose"
 import memeModel from "../models/memeModel.js ";
 
 const getAllMemes = async (req, res) => {
-    const allMemes = await memeModel.find();
-    console.log('allMemes', allMemes),
-        res.json({
-            number: allMemes.lenght,
-            allMemes
-        })
+    try {
+        const allMemes = await memeModel.find();
+        console.log("allMemes", allMemes);
+        if
+            (allMemes.length < 1) {
+            res.status(404).json({ message: "No memes found" });
+        } else {
+            res.status(200).jason({ number: allMemes.length, allMemes: allMemes });
+        }
+
+    } catch (error) {
+        console.log('error', error);
+        res.status(500).jason({ error: "something went wrong" })
+    }
 };
 
 export { getAllMemes };
+
